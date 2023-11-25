5d0f2a2dadfe9fdb47c4a6d40ed6a441fb62e7e7
“Saphron” <109723086+denerose@users.noreply.github.com>
2023-11-25 14:18:49 +1100

    Increase contrast on hover

--- src/CatCard.jsx
+++ src/CatCard.jsx
@@ -5,9 +5,8 @@ export function CatCard({ url, id, handleClick }) {
 
     return (
         <>
-            <p>{id}</p>
-            <div onClick={() => handleClick(id)} className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg">
-                <img src={url} className="object-cover group-hover:opacity-75" />
+            <div onClick={() => handleClick(id)} className="group box-border border-white aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gradient-to-t from-slate-900 to-slate-300">
+                <img src={url} className="object-cover group-hover:opacity-60 " />
             </div>
         </>
     );
